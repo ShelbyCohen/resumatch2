e496f949c7a9b4b426c325085b06a8627af3af71
Shelby Cohen <s.cohen131@gmail.com>
2016-03-07 17:36:09 -0500

    adding files

---
+++ resumatch2/resumatch2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  resumatch2
+//
+//  Created by S Cohen on 3/7/16.
+//  Copyright © 2016 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ resumatch2/resumatch2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  resumatch2
+//
+//  Created by S Cohen on 3/7/16.
+//  Copyright © 2016 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
