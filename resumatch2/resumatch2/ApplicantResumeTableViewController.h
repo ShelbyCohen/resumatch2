7bcb3d2679433d61802a467352a1c2b04c2c7a1c
Shelby Cohen <s.cohen131@gmail.com>
2016-04-21 17:08:50 -0400

    added table view controllers for matches

---
+++ resumatch2/resumatch2/ApplicantResumeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ApplicantResumeTableViewController.h
+//  resumatch2
+//
+//  Created by S Cohen on 4/21/16.
+//  Copyright © 2016 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ApplicantResumeTableViewController : UITableViewController
+
+@end
---
+++ resumatch2/resumatch2/RecruiterResumeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecruiterResumeTableViewController.h
+//  resumatch2
+//
+//  Created by S Cohen on 4/21/16.
+//  Copyright © 2016 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecruiterResumeTableViewController : UITableViewController
+
+@end
