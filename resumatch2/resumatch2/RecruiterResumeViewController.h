2289ace8656d2805fa2539f043b07e775bf75bd7
Caitlin Wormsly <caitlinwormsly@Alis-MacBook-Pro.local>
2016-05-03 21:35:10 -0400

    Added a placeholder resume and thumbs up/down buttons

---
+++ resumatch2/resumatch2/RecruiterResumeViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecruiterResumeViewController.h
+//  resumatch2
+//
+//  Created by Caitlin Wormsly on 5/3/16.
+//  Copyright © 2016 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecruiterResumeViewController : UIViewController
+
+@end
---
+++ resumatch2/resumatch2/RecruiterResumeViewController.m
@@ -0,0 +1,38 @@
+//
+//  RecruiterResumeViewController.m
+//  resumatch2
+//
+//  Created by Caitlin Wormsly on 5/3/16.
+//  Copyright © 2016 Apress. All rights reserved.
+//
+
+#import "RecruiterResumeViewController.h"
+
+@interface RecruiterResumeViewController ()
+
+@end
+
+@implementation RecruiterResumeViewController
+
+- (void)viewDidLoad {
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+@end
